8f67bae35c9e0cfcd57b8259080da07843ca2c4c
Miriam Rozenbaum <miriamrozenbaum@Miriams-MacBook-Pro.local>
2017-05-01 09:31:42 -0500

    #11 done

--- ChinookDatabase1.4_CompleteVersion/line_items_per_invoice.sql
+++ ChinookDatabase1.4_CompleteVersion/line_items_per_invoice.sql
@@ -1,1 +1,5 @@
-/*line_items_per_invoice.sql: Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY */
+/*Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for each Invoice. HINT: GROUP BY*/
+select invoice.invoiceId, count(invoiceLine.invoiceId) as
+NumberOfLines from invoice
+join invoiceLine on invoice.invoiceId=invoiceLine.invoiceId
+group by invoiceLine.invoiceId;
